7f022e0230359914e1f417e17cc351a0a833fb68
Jehwan-Kwak <jehwane@naver.com>
2023-01-30 00:11:27 +0900

    after #2 recap2

---
+++ app.js
@@ -0,0 +1,10 @@
+
+const player = {
+    name: "jehwan",
+    sayHello: function (otherPersonsName) {
+        console.log("hello! " + otherPersonsName + " nice to meet you!")
+    },
+};
+
+console.log(player.name);
+player.sayHello("edo");
