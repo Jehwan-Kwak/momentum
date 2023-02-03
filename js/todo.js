944d629ddcb253e0712b059268876472b17558a3
Jehwan-Kwak <jehwane@naver.com>
2023-02-04 00:18:39 +0900

    done

--- js/todo.js
+++ js/todo.js
@@ -13,17 +13,21 @@ function saveToDos(){
 function deleteToDo (event) {
     const li = event.target.parentElement;
     li.remove();    
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+    saveToDos();
 }
 
 function paintToDo(newToDo) {
     const li = document.createElement("li");
+    li.id = newToDo.id;
     const span = document.createElement("span");
+    span.innerText = newToDo.text;
     const button = document.createElement("button");
     button.innerText = "X";
     button.addEventListener("click", deleteToDo)
     li.appendChild(span);
     li.appendChild(button);
-    span.innerText = newToDo;
+    
     
     toDoList.appendChild(li);
 }
@@ -32,8 +36,12 @@ function handleToDoSubmit (event) {
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newToDo);
-    paintToDo(newToDo);
+    const newToDoObj = {
+        text:newToDo,
+        id: Date.now(),
+    }
+    toDos.push(newToDoObj);
+    paintToDo(newToDoObj);
     saveToDos();
 }
 
